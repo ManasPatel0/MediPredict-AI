0b8dc0b03b146ee1c7047a88ff2eb967e8091f78
ManasPatel0 <152642361+ManasPatel0@users.noreply.github.com>
2026-07-29 10:13:25 +0530

    Implement JWT authentication and protected user endpoints

--- backend/app/routes/user.py
+++ backend/app/routes/user.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from app.database.database import get_db
 from app.models.user import User
 from app.schemas.user import UserCreate, UserResponse
-from app.utils.security import hash_password
+from app.utils.security import hash_password, verify_password
+from app.auth.jwt_handler import create_access_token
+from app.auth.dependencies import get_current_user
 
 router = APIRouter()
 
@@ -28,3 +31,34 @@ def signup(user: UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return new_user
+
+
+@router.post("/login")
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
+):
+
+    existing_user = db.query(User).filter(
+        User.email == form_data.username
+    ).first()
+
+    if not existing_user:
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+
+    if not verify_password(form_data.password, existing_user.password):
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+
+    access_token = create_access_token(
+        data={"sub": existing_user.email}
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
+
+
+@router.get("/me", response_model=UserResponse)
+def get_me(current_user: User = Depends(get_current_user)):
+    return current_user
